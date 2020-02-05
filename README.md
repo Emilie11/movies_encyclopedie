1148414e894fef4185ab6da0fde6c6e8f253de10
Emilie11 <emiliephilippe78@gmail.com>
2020-02-05 09:51:23 +0100

    premiére version fonctionelle sans habillage et avec des éléments manquants

---
+++ README.md
@@ -0,0 +1,17 @@
+# Mes films
+
+## How launch it ? 
+
+-> Create a .env file on root with thoses elements:
+
+	FLASK_APP=main.py
+	FLASK_ENV=development
+	API_KEY=[YOUR_API_KEY]
+
+The install depedencies 
+
+	pip install -r requirements.txt
+
+And launch it 
+
+	flask run
---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, session, request
+from dotenv import load_dotenv
+import os, json
+
+load_dotenv()
+app = Flask(__name__)
+
+
+
+# default page
+@app.route('/')
+@app.route('/index')
+def index():	
+	return render_template('search.html', title='Home')
+
+
+@app.route('/search', methods=['POST'])
+def search_movie():
+	search_term = request.form['search_term']
+
+	####### DEVELOPMENT PURPOSE #######
+	search_result = json.load(open('./examples/search.json', 'rb'))['Search']
+
+	return render_template('search.html', title='Home', movies_found=search_result)
+
+@app.route('/movie/<string:movie_id>')
+def movie_page(movie_id):
+
+	####### DEVELOPMENT PURPOSE #######
+	card_result = json.load(open('./examples/card.json', 'rb'))
+
+	return render_template('card.html', title='Home', card = card_result)
+
+
+if __name__ == "__main__":
+	app.run()
