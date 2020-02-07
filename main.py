748938169f4d0af4278feeb2d1eef3349c468a39
Emilie11 <emiliephilippe78@gmail.com>
2020-02-07 15:53:05 +0100

    Ready to production !

--- main.py
+++ main.py
@@ -18,15 +18,15 @@ def index():
 @app.route('/search', methods=['POST'])
 def search_movie():
 	search_term = request.form['search_term']
-
+	
 	####### DEVELOPMENT PURPOSE #######
-	search_result = json.load(open('./examples/search.json', 'rb'))['Search']
+	# search_result = json.load(open('./examples/search.json', 'rb'))['Search']
 
 	####### PRODUCTION PURPOSE  #######
 	api_key = os.getenv("API_KEY")
-	# url = 'http://www.omdbapi.com/?apikey=' + api_key + '&s=' + search_term
-	# search_result = requests.get(url).json()
-	# search_result = search_result['Search']
+	url = 'http://www.omdbapi.com/?apikey=' + api_key + '&s=' + search_term
+	search_result = requests.get(url).json()
+	search_result = search_result.get('Search')
 
 
 	return render_template('search.html', title='Home', movies_found=search_result)
@@ -35,12 +35,12 @@ def search_movie():
 def movie_page(movie_id):
 
 	####### DEVELOPMENT PURPOSE #######
-	card_result = json.load(open('./examples/card.json', 'rb'))
+	# card_result = json.load(open('./examples/card.json', 'rb'))
 
 	####### PRODUCTION PURPOSE  #######
-	# api_key = os.getenv("API_KEY")
-	# url = 'http://www.omdbapi.com/?apikey=' + api_key + '&i=' + movie_id
-	# card_result = requests.get(url).json()
+	api_key = os.getenv("API_KEY")
+	url = 'http://www.omdbapi.com/?apikey=' + api_key + '&i=' + movie_id
+	card_result = requests.get(url).json()
 
 	return render_template('card.html', title='Home', card = card_result)
 
